29841c1d328455e47438dd0412d079ce9efe1753
ErickMartinez33 <63166685+ErickMartinez33@users.noreply.github.com>
2020-04-05 18:42:41 -0500

    Initial commit

---
+++ app/src/main/java/com/example/mapas/activities/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.mapas.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.mapas.R;
+import com.example.mapas.fragments.MapFragment;
+import com.example.mapas.fragments.WelcomeFragment;
+
+public class MainActivity extends AppCompatActivity {
+    Fragment currentFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            currentFragment = new WelcomeFragment();
+            changeFragment(currentFragment);
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_welcome:
+                currentFragment = new WelcomeFragment();
+                break;
+            case R.id.menu_map:
+                currentFragment = new MapFragment();
+                break;
+        }
+        changeFragment(currentFragment);
+        return super.onOptionsItemSelected(item);
+    }
+    private void changeFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragement_container, fragment).commit();
+    }
+
+}
